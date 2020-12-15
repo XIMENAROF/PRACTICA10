c5990f4b6dd21d0930d5f1005ef2f5cd69bfa4e6
ximena robledo <54002609+XIMENAROF@users.noreply.github.com>
2020-12-14 20:02:22 -0600

    Create practica 10.1.c

---
+++ practica 10.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int  main() {
+
+
+int N, CONT, AS;
+AS=0;
+CONT=1;
+
+printf("TECLEA UN NUMERO: ");
+scanf("%i",&N);
+    while(CONT<=N){
+    AS=(AS+CONT);
+     CONT=(CONT+2);
+}
+       printf("\nEL RESULTADO ES %i\n", AS);   
+
+return 0;
+}
